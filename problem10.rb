c6d3f4c37c90593adc5572126308f8db61832bc5
Justin Stone <jclstone@gmail.com>
2014-02-13 10:14:52 -0800

    added solution for problem 10

---
+++ problem10.rb
@@ -0,0 +1,29 @@
+#Find the sum of all the primes below two million.
+
+class SumPrimes
+
+  def is_prime?(number) 
+    square = Math.sqrt(number)
+    for i in 2..square
+      if number % i == 0
+        return false
+        break
+      end
+    end
+    return true
+  end
+
+  def sum_primes(number)
+    result = 0
+    for i in 2...number
+      if is_prime?(i)
+        #puts i
+        result += i
+      end
+    end
+    puts result
+  end
+end
+
+check = SumPrimes.new
+check.sum_primes(2000000)
